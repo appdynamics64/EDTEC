a0cc6d289b31b66a9029e9afc9427103c77de9d9
Appmocku <appdynamics64@gmail.com>
2025-02-14 23:04:09 +0530

    push

--- my-app/src/reportWebVitals.js
+++ my-app/src/reportWebVitals.js
@@ -1,10 +1,12 @@
-import { onCLS, onFID, onLCP } from 'web-vitals';
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    onCLS(onPerfEntry);
-    onFID(onPerfEntry);
-    onLCP(onPerfEntry);
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
